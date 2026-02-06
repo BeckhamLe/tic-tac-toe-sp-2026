6d7840e981ff9a86fecc57cd22654d237fb9aeb4
Beckham Le <beckhamle@Beckhams-MacBook-Pro.local>
2026-02-06 03:40:09 -0500

    Finished implementing api into frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-import { useState, useEffect } from "react";
-import { type GameState } from "./tic-tac-toe"; // Import GameState type for gameboard cells
-import gameServices from './services/game.ts'   // Import game services to interact with backend API
-import './App.css'  // Import CSS for appstyling
+import { useState } from "react";
+import LobbyView from "./components/LobbyView.tsx";
+import GameView from "./components/GameView.tsx";
 
 function App() {
-  const [gameState, setGameState] = useState<GameState | null>(null)
-  const [gameWinner, setGameWinner] = useState(null) 
+  const [selectedGameId, setSelectedGameId] = useState<string | null>(null)    // game id state; also used as flag for determining lobby or game view
 
+  /*
   // Checks for winner or draw after every move (gameState change)
   useEffect(() => {
     // If game state doesn't exist yet
     if(!gameState){
       // use services to send a request and return back response in json format
-      gameServices.getGame().then((initialGameState) => {
+      gameServices.createGame().then((initialGameState) => {
         setGameState(initialGameState)  // update game state to initial game state returned from server
       })
     } else if(gameWinner === null && !gameState.board.includes(null)){  // check for draw (if no winner and no nulls left on board)
@@ -28,49 +27,12 @@ function App() {
       <p>Loading</p>
     )
   }
-
-  // Handles a player's move when they click on a cell
-  const handleClick = (position: number) => {
-    // Call the move service to make a move at the specified position
-    gameServices.move(position).then((response) => {
-      setGameState(response.gameState)  // update game state to new game state returned from server
-      setGameWinner(response.winner)    // update game winner
-    })
-  }
-
-  // Resets the game to its initial state
-  const resetGame = () => {
-    gameServices.reset().then((newGame) => {
-      setGameState(newGame)
-      setGameWinner(null)
-    })
-  }
-
+  */
   // TODO: display the gameState, and call `makeMove` when a player clicks a button
   return (
-    <div className="object_app">
-      <p className="subheading_app">Current Player: {gameState.currentPlayer}</p>
-      <table>
-        <tbody>
-          <tr id="top_row">
-            <td onClick={() => handleClick(0)}>{gameState.board[0]}</td>
-            <td className="middle_column" onClick={() => handleClick(1)}>{gameState.board[1]}</td>
-            <td onClick={() => handleClick(2)}>{gameState.board[2]}</td>
-          </tr>
-          <tr id="middle_row">
-            <td onClick={() => handleClick(3)}>{gameState.board[3]}</td>
-            <td className="middle_column" onClick={() => handleClick(4)}>{gameState.board[4]}</td>
-            <td onClick={() => handleClick(5)}>{gameState.board[5]}</td>
-          </tr>
-          <tr id="bottom_row">
-            <td onClick={() => handleClick(6)}>{gameState.board[6]}</td>
-            <td className="middle_column" onClick={() => handleClick(7)}>{gameState.board[7]}</td>
-            <td onClick={() => handleClick(8)}>{gameState.board[8]}</td>
-          </tr>
-        </tbody>
-      </table>
-      <button onClick={resetGame}>Reset</button>
-    </div>
+    selectedGameId === null
+      ? <LobbyView onSelectGame={(gameId: string) => setSelectedGameId(gameId)} />
+      : <GameView gameId={selectedGameId} onBack={() => setSelectedGameId(null)} />
   );
 }
 
--- src/services/game.ts
+++ src/services/game.ts
@@ -1,21 +1,34 @@
 import axios from 'axios'
 const baseUrl = 'http://localhost:3001'
 
-// Request to get initial game
-const getGame = () => {
-    const request = axios.get(`${baseUrl}/game`)
-    return request.then((response) => response.data)    // returns data of initial game state
+// Request to create a new game state
+const createGame = () => {
+    const request = axios.post(`${baseUrl}/create`)
+    return request.then((response) => response.data)
+}
+
+// Request to get an existing game state
+const getGame = (gameId: string) => {
+    const request = axios.get(`${baseUrl}/games/${gameId}`)
+    return request.then((response) => response.data)    // returns data of game state associated with id
+}
+
+// Request to get an array of all game ids
+const getAllGames = () => {
+    const request = axios.get(`${baseUrl}/games`)
+    return request.then((response) => response.data)
 }
 
 // Request to post move to game
-const move = (position: number) => {
-    const request = axios.post(`${baseUrl}/move`, {position})   // pass position in body of post request so no weird errors occur
+const move = (gameId: string, position: number) => {
+    const request = axios.post(`${baseUrl}/move/${gameId}`, {position})   // pass position in body of post request so no weird errors occur
     return request.then((response) => response.data)    // returns data of updated game state and winner (if any)
 }
 
-const reset = () => {
-    const request = axios.post(`${baseUrl}/reset`)
+// Request to reset state of game currently selected
+const reset = (gameId: string) => {
+    const request = axios.post(`${baseUrl}/reset/${gameId}`)
     return request.then((response) => response.data)    // returns data of reset game state
 }
 
-export default { getGame, move, reset }
+export default { createGame, getGame, getAllGames, move, reset }
